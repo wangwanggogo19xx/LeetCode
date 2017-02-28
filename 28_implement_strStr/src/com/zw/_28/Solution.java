f7a4d055b8716ac91f0d4a2cb872b4f129bf5353
zwanggogo <zw@zw.local>
2017-02-28 19:48:38 +0800

    20170228

---
+++ 28_implement_strStr/src/com/zw/_28/Solution.java
@@ -0,0 +1,23 @@
+package com.zw._28;
+
+public class Solution {
+	public int strStr(String haystack,String needle){
+		if(haystack.startsWith(needle)){
+			return 0;
+		}
+		for(int i=0;i<haystack.length()-needle.length()+1;i++){
+			int j;
+			int k=i;
+			for(j=0;j<needle.length();j++){
+				if(haystack.charAt(k) != needle.charAt(j)){
+					break;
+				}
+				k++;
+			}
+			if (j==needle.length()){
+				return i;
+			}
+		}
+		return -1;
+	}
+}
